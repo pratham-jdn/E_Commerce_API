392986b18f57bc388ac02821407271042b50694b
pratham-jdn <2003prathamkumar@gmail.com>
2025-12-24 22:09:56 +0530

    preflight issue

--- server.js
+++ server.js
@@ -1,42 +1,53 @@
-import express from 'express'
-import mongoose from 'mongoose';
-import bodyParser from 'express'
-import userRouter from './Routes/user.js'
-import productRouter from './Routes/product.js'
-import cartRouter from './Routes/cart.js'
-import addressRouter from './Routes/address.js'
-import paymentRouter from './Routes/payment.js'
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "express";
+import cors from "cors";
+
+import userRouter from "./Routes/user.js";
+import productRouter from "./Routes/product.js";
+import cartRouter from "./Routes/cart.js";
+import addressRouter from "./Routes/address.js";
+import paymentRouter from "./Routes/payment.js";
 import adminUserRoutes from "./Routes/adminUser.js";
 import adminProductRoutes from "./Routes/adminProduct.js";
-import cors from 'cors';
 
 const app = express();
 
-app.use(bodyParser.json())
 
-app.use(cors({
-  origin:true,
-  methods:[ "GET","POST","PUT","DELETE"],
-  credentials:true
-}))
+app.use(bodyParser.json());
+
+
+app.use(
+  cors({
+    origin: true, // allow all origins 
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Auth"],
+    credentials: true,
+  })
+);
+
+app.options("*", cors());
+
 
 // home testing route
-app.get('/',(req,res)=>res.json({messge:'This is home route'}))
+app.get("/", (req, res) =>
+  res.json({ message: "This is home route" })
+);
 
 // user Router
-app.use('/api/user',userRouter)
+app.use("/api/user", userRouter);
 
 // product Router
-app.use('/api/product',productRouter)
+app.use("/api/product", productRouter);
 
 // cart Router
-app.use('/api/cart',cartRouter)
+app.use("/api/cart", cartRouter);
 
 // address Router
-app.use('/api/address',addressRouter)
+app.use("/api/address", addressRouter);
 
 // payment Router
-app.use('/api/payment',paymentRouter)
+app.use("/api/payment", paymentRouter);
 
 // admin user routes
 app.use("/api/admin", adminUserRoutes);
@@ -45,12 +56,14 @@ app.use("/api/admin", adminUserRoutes);
 app.use("/api/admin", adminProductRoutes);
 
 mongoose
-  .connect(
-    process.env.MONGO_URI,
-    { dbName: "MERN_E_Commerce" }
-  )
-  .then(() => console.log("MongoDB Connected Succssfully...!"))
+  .connect(process.env.MONGO_URI, {
+    dbName: "MERN_E_Commerce",
+  })
+  .then(() => console.log("MongoDB Connected Successfully"))
   .catch((err) => console.log(err));
 
-const port = 1000;
-app.listen(port,()=>console.log(`Server is running on port ${port}`))
+
+const PORT = process.env.PORT || 1000;
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
